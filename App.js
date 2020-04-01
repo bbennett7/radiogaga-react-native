bee0e7c13eb8b57d9ab265f0abb9ae74d700b40a
bbennett7 <bryn.bennett@gmail.com>
2020-03-31 18:01:19 -0700

    Landing screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import COLORS from './src/shared/style/colors';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.logo}>radio gaga</Text>
     </View>
   );
 }
@@ -12,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.rgRed,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  logo: {
+    color: COLORS.rgBlueGrey,
+    fontFamily: 'SpicyRice-Regular',
+    fontSize: 60
+  }
 });
